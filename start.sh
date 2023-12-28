d6ff14aa1b738949a0b93c1b3db30db6fe22de4d
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-12-28 19:40:51 +0700

    Updated

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 
 python manage.py flush --no-input
-python manage.py makemigrations --no-input
+python manage.py makemigrations --merge --no-input
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:80
